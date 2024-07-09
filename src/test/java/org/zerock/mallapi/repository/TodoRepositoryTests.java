376cdf0070fe2a03f7613dbec2237b1cc05585d2
shinis(green) <actionshin@gmail.com>
2024-07-09 16:09:54 +0900

    98p todo 엔티티  테스트 코드

--- src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
+++ src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -1,8 +1,17 @@
 package org.zerock.mallapi.repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.mallapi.domain.Todo;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -14,8 +23,66 @@ public class TodoRepositoryTests {
 	private TodoRepository todoRepository;
 
 	@Test
-	public void test1() {
+	public void testInsert() {
 		log.info("----");
 		log.info(todoRepository);
+		
+		for (int i = 1; i <= 100; i++) {
+			Todo todo = Todo.builder()
+					.title("Title..." + i)
+					.dueDate(LocalDate.of(2023,12,31))
+					.writer("user00")
+					.build();
+			todoRepository.save(todo);
+		}
 	}
+	
+	@Test
+	public void testRead() {
+		// 존재하는 번호를 확인
+		Long tno = 33L;
+		
+		Optional<Todo> result = todoRepository.findById(tno);
+		
+		Todo todo = result.orElseThrow();
+		
+		log.info(todo);
+	}
+	
+	@Test
+	public void testModify() {
+		
+		Long tno = 33L;
+		
+		Optional<Todo> result = todoRepository.findById(tno);
+		
+		Todo todo = result.orElseThrow();
+		todo.changeTitle("Modified 33...");
+		todo.changeComplete(true);
+		todo.changeDueDate(LocalDate.of(2023, 10, 10));
+		
+		todoRepository.save(todo);
+	}
+	
+	@Test
+	public void testDelete() {
+
+
+		Long tno = 33L;
+		
+		todoRepository.deleteById(tno);
+	}
+	
+	@Test
+	public void testPaging() {
+
+		Pageable pageable = PageRequest.of(0, 10, Sort.by("tno").descending());
+		
+		Page<Todo> result = todoRepository.findAll(pageable);
+		
+		log.info(result.getTotalElements());
+		
+		result.getContent().stream().forEach(todo -> log.info(todo));
+	}
+	
 }
